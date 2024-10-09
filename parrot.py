a7ec2d5e77abfedb6a922b09c9929f70080bf31f
nabila ahmed <sumayaad2020@gmail.com>
2024-10-09 18:59:40 +0600

    ab

---
+++ parrot.py
@@ -0,0 +1,21 @@
+class parrot:
+    species ="bird"
+    
+    def __init__(self,name,age):
+        self.name =name
+        self.age =age
+        
+        
+blu =parrot("Blu",10)
+woo =parrot("woo",15)
+
+print(f"Blue is a {blu.species}")
+print(f"woo is a {woo.species}")
+
+print(f"{blu.name} is {blu.age} years old")
+print(f"{woo.name} is {woo.age} years old")
+
+
+
+
+        
